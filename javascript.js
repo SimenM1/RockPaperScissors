ef0ed8601269e1479a39163930e2bd761b8ac8fc
SimenM1 <156329770+SimenM1@users.noreply.github.com>
2024-01-21 23:38:46 +0000

    wrapped in function

--- javascript.js
+++ javascript.js
@@ -1,36 +1,40 @@
-function compChoice() {    
-    const objects = ["rock", "paper", "scissors"]
-    let compnr = Math.floor (Math.random()*3)
-    return objects[compnr]
-}
+function playRound(){    
+    function compChoice() {    
+        const objects = ["rock", "paper", "scissors"]
+        let compnr = Math.floor (Math.random()*3)
+        return objects[compnr]
+    }
 
-let playerChoice = prompt("Choose Rock Paper or Scissors!").toLowerCase()
-let computerChoice = compChoice()
-console.log(playerChoice)
-console.log(computerChoice)
-while (computerChoice === playerChoice) {
-    console.log("draw")
-    playerChoice = prompt("Choose Rock Paper or Scissors!").toLowerCase()
-    computerChoice = compChoice()
+    let playerChoice = prompt("Choose Rock Paper or Scissors!").toLowerCase()
+    let computerChoice = compChoice()
     console.log(playerChoice)
     console.log(computerChoice)
+    while (computerChoice === playerChoice) {
+        console.log("draw")
+        playerChoice = prompt("Choose Rock Paper or Scissors!")
+        computerChoice = compChoice()
+        console.log(playerChoice)
+        console.log(computerChoice)
+    }
+
+    if (computerChoice === "rock" && playerChoice === "paper") {
+            return "you win"
+
+        }
+
+        else if (computerChoice === "paper" && playerChoice === "scissors") {
+            return "you win"
+
+        }
+
+        else if (computerChoice === "scissors" && playerChoice === "rock") {
+            return "you win"
+
+        }
+
+        else {
+            return "you lose!!!!"
+        }
 }
 
-if (computerChoice === "rock" && playerChoice === "paper") {
-        console.log("you win")
-
-    }
-
-    else if (computerChoice === "paper" && playerChoice === "scissors") {
-        console.log("you win")
-
-    }
-
-    else if (computerChoice === "scissors" && playerChoice === "rock") {
-        console.log("you win")
-
-    }
-
-    else {
-        console.log("you lose!")
-    }
+console.log(playRound())
